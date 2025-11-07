96ede07fafd1ff118c100676364c8e1fe37fa427
micwin2135 <micwin2135@students.ecpi.edu>
2025-11-07 18:37:49 -0500

    Create App.java

---
+++ App.java
@@ -0,0 +1,24 @@
+/********************************************
+ * Name: Michael Winstead
+ * Date: 10/29/2025
+ * Purpose: Assignment SDC330L project 3.2
+ */
+public class Week3EmployeeApp {
+    public static void main(String[] args) {
+        EmployeeManager manager = new EmployeeManager();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== Week 3 Employee Management Application ===");
+        System.out.println("Author: Your Name");
+        System.out.println("Welcome! Use the menu to manage employees.\n");
+
+        // Example data
+        manager.addEmployee(new SalariedEmployee(1, "Alice", 60000));
+        manager.addEmployee(new HourlyEmployee(2, "Bob", 20, 160));
+        manager.addEmployee(new CommissionEmployee(3, "Charlie", 5000, 0.10));
+
+        manager.displayAllEmployees();
+
+        sc.close();
+    }
+}
