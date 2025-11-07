df0140fade372f6189bac4a03da470d49d1e799b
micwin2135 <micwin2135@students.ecpi.edu>
2025-11-07 18:45:30 -0500

    Create EmployeeManager.java

---
+++ EmployeeManager.java
@@ -0,0 +1,46 @@
+/********************************************
+ * Name: Michael Winstead
+ * Date: 10/29/2025
+ * Purpose: Assignment SDC330L project 3.2
+ */
+class EmployeeManager {
+    private ArrayList<Employee> employees = new ArrayList<>();
+
+    public void addEmployee(Employee e) {
+        employees.add(e);
+        System.out.println("Employee added successfully!");
+    }
+
+    public void removeEmployee(int id) {
+        employees.removeIf(e -> e.getId() == id);
+        System.out.println("Employee removed successfully!");
+    }
+
+    public void updateEmployee(int id, Employee newData) {
+        for (int i = 0; i < employees.size(); i++) {
+            if (employees.get(i).getId() == id) {
+                employees.set(i, newData);
+                System.out.println("Employee updated successfully!");
+                return;
+            }
+        }
+        System.out.println("Employee not found!");
+    }
+
+    public void displayAllEmployees() {
+        for (Employee e : employees) {
+            e.displayInfo();
+            System.out.println("-----");
+        }
+    }
+
+    public void displayEmployeesByType(Class type) {
+        for (Employee e : employees) {
+            if (type.isInstance(e)) {
+                e.displayInfo();
+                System.out.println("-----");
+            }
+        }
+    }
+}
+
